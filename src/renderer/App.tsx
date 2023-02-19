805406b8135f753e8315aab2b6c8843b2f6da0b4
Felix Leclerc Jr <felixLJR@icloud.com>
2023-02-18 19:34:42 -0500

    tailwind config done

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,11 +1,10 @@
 
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
     <div>
-     <h1>Test</h1> 
+     <h1>Test1</h1> 
     </div>
   );
 }
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
 import { createRoot} from 'react-dom/client';
-import './App.css';
+import './dist/output.css';
 import App from './App';
 
 const root = createRoot(
   document.getElementById('root') as HTMLElement
 );
 console.log(root);
-console.log(App);
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <App />
-  // </React.StrictMode>
+  </React.StrictMode>
 );
-
-// createRoot(document.querySelector('#root')).render(
-//   <App />
-// )
-
-// import { createRoot } from 'react-dom/client';
-// const container = document.getElementById('app');
-// const root = createRoot(container); // createRoot(container!) if you use TypeScript
-// root.render(<App tab="home" />);
-// createRoot(document.querySelector('#App')).render(
