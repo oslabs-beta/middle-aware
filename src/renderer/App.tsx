a9a51549f424b748c17760119c302e7533a74d1d
Felix Leclerc Jr <felixLJR@icloud.com>
2023-03-29 22:13:10 -0400

    app.tsx cleaned and commented, before fixing error with ERROR

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import ResultCards from './components/ResultCards'
 import RouteCards from './components/RouteCards'
 import AwaitingInput from './components/AwaitingInput'
 import { Responses, APIfuncs } from './Types'
 
-// add fx to preload (contextIsolation) to prevent end users from reaching electron API
 declare global {
   interface Window {
     electronAPI: APIfuncs;
@@ -12,28 +11,25 @@ declare global {
 }
 
 function App () {
-  let getTests: Responses[]
-
+  // This will store the lastest test retrieved from the fetchFromDB function below
   const [results, setResults] = useState<Responses[]>([])
+  // allRoutes will store all routes from the dB, this is used to populate the Routes card in the GUI
   const [allRoutes, setAllRoutes] = useState<{detail: string, last_test_id: string}[]>([])
+  // this is used to store all the routes found by parseAPIRequest
+  const [fetchResources, setResources] = useState<string[]>([])
 
+  // fetchTestFromDB fetches the tests associated with the endpoint that is selected on the left hand side of the app; this is used to render the Result cards
   const fetchTestsFromDB = (id: string) => {
-    console.log('test id passed in app:', id)
+    let getTests: Responses[]
     window.electronAPI
       .getTest(id)
       .then((data: string) => {
-        // console.log(data)
         getTests = JSON.parse(data)
-        console.log(getTests)
         setResults(getTests)
       })
       .catch((err: unknown) => console.log('Problem with db Tests:', err))
   }
-
-  // Find the right mouse event instead of any!!!!!!
-  // type eventID = {
-  //   id: string
-  // }
+  // Each route card on the left is a div, when clicked the divs render the result cards on the right; fetchTestsFromDB is invoked for the test data
   const resultHandler = (event: React.MouseEvent<HTMLDivElement>) => {
     event.preventDefault()
     let testToFilter = (event.target as HTMLDivElement).id // path name
@@ -48,49 +44,34 @@ function App () {
     console.log('testToFilter: ', testToFilter)
     // tried hard coding:
     fetchTestsFromDB(testToFilter)
-    // for (const test of getTests) {
-    //   if (test.detail === testToFilter) {
-    //     filteredTests.push(test)
-    //   }
   }
-
-  const [fetchResources, setResources] = useState<string[]>([])
-
+  // select a directory button to select a path
   const handleButtonClick = () => {
     window.electronAPI
       .openFile()
       .then((result: string) => {
-        // const temp = window.electronAPI.openFile().then((result) => {
         // Expect result to be a directory
         window.electronAPI
           .parseFiles(result)
           .then((result: string[]) => {
             // Expect result to be an array of fetch resources
-            console.log('handlebuttonclick/fetchFromResources', result)
             setResources(result) //    return result;
           })
           .catch((err: unknown) => console.log('parseFiles Error:', err))
       })
-      .catch((err: unknown) => console.log('openFile Error: ', err)) // .then()? Maybe save this as temp and chain open file and parse file in one here;
+      .catch((err: unknown) => console.log('openFile Error: ', err))
   }
 
+  // triggered by 'Look for test data' button
   const fetchFromDB = () => {
     window.electronAPI
       .getAllRoutes()
       .then((data: string) => {
+        // save all the routes to setAllRoutes
         setAllRoutes(JSON.parse(data))
-        // console.log('fetchfromDB: getallRoutes', getAllRoutes)
       })
       .catch((err: unknown) => console.log('Problem with db Routes:', err))
   }
-
-  useEffect(() => {
-    // Console log routes for debugging
-    console.log('routes: ', allRoutes)
-  })
-
-  // this is for the key in the routes being rendered
-  let index = 0
 
   return (
     <>
@@ -108,7 +89,7 @@ function App () {
       <div id="main">
         <div id='routesSection'>
           <h2 className='title'>Routes</h2>
-
+          {/* iterate through routes */}
           {fetchResources.map((routes: string) => {
             const databaseRoutes: string[] = []
             for (const routeData of allRoutes) {
@@ -117,8 +98,7 @@ function App () {
             const available = databaseRoutes.includes(routes)
             const error = (routes === '/api/cards')
             return (
-              // key is using index above because there is no id; try to use the element index
-              <RouteCards id={routes} detail={routes} onClick={resultHandler} key={index++} available={available} error={error}/>
+              <RouteCards id={routes} detail={routes} onClick={resultHandler} key={fetchResources.indexOf(routes)} available={available} error={error}/>
             )
           })}
 
