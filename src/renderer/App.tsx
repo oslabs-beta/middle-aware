b4aa6ef525d99da8aa17eb20045bf4d04607de57
jason-breen <jason.breen@gmail.com>
2023-04-01 23:50:23 -0400

    Merge in files from branch felix_justin/mid-58-typescript-conversion for edits to front end handling of file parse data

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,140 +1,141 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import ResultCards from './components/ResultCards'
 import RouteCards from './components/RouteCards'
-import AwaitingInput from './components/AwaitingInput'
+import { Responses, APIfuncs } from './Types'
 
-// add fx to preload (contextIsolation) to prevent end users from reaching electron API
+
 declare global {
   interface Window {
-    electronAPI: any;
+    electronAPI: APIfuncs;
   }
 }
 
 function App () {
-  let getTests: any
+  // This will store the lastest test retrieved from the fetchFromDB function below
+  const [results, setResults] = useState<Responses[]>([])
+  // allRoutes will store all routes from the dB, this is used to populate the Routes card in the GUI
+  const [allRoutes, setAllRoutes] = useState<{detail: string, last_test_id: string}[]>([])
+  // this is used to store all the routes found by parseAPIRequest
+  const [fetchResources, setResources] = useState<string[]>([])
 
-  const [results, setResults] = useState<any>([])
-  const [allRoutes, setAllRoutes] = useState<any>([])
+  // const [routeAndResultVisibility, setRouteAndResultVisibility] = useState(false)
 
+  // fetchTestFromDB fetches the tests associated with the endpoint that is selected on the left hand side of the app; this is used to render the Result cards
   const fetchTestsFromDB = (id: string) => {
-    console.log('test id passed in app:', id)
+    let getTests: Responses[]
     window.electronAPI
       .getTest(id)
-      .then((data: any) => {
-        // console.log(data)
+      .then((data: string) => {
         getTests = JSON.parse(data)
-        console.log(getTests)
         setResults(getTests)
       })
-      .catch((err: any) => console.log('Problem with db Tests:', err))
+      .catch((err: unknown) => console.log('Problem with db Tests:', err))
   }
-
-  // Find the right mouse event instead of any!!!!!!
-  const resultHandler = (event: any) => {
+  // Each route card on the left is a div, when clicked the divs render the result cards on the right; fetchTestsFromDB is invoked for the test data
+  const resultHandler = (event: React.MouseEvent<HTMLDivElement>) => {
     event.preventDefault()
-    let testToFilter = ''
-    // let filteredTests: string[] = [];
-    testToFilter = event.target.id // path name
-
+    let testToFilter = (event.target as HTMLDivElement).id // path name from parseAPIReq.js
+    // this will stay as is. consider adding a popover or something to explain template literals!!!!!!
     for (const item of allRoutes) {
       // item.detail (e.g. /api/auth/user/)
       const temp = (item.detail.charAt(item.detail.length - 1) === '/' ? item.detail.slice(0, item.detail.length - 2) : item.detail)
+      // if path from div === cleaned up temp
       if (testToFilter === temp) {
         testToFilter = item.last_test_id
       }
     }
     console.log('testToFilter: ', testToFilter)
-    // tried hard coding:
     fetchTestsFromDB(testToFilter)
-    // for (const test of getTests) {
-    //   if (test.detail === testToFilter) {
-    //     filteredTests.push(test)
-    //   }
   }
 
-  const [fetchResources, setResources] = useState([])
+  // triggered by 'Look for test data' button
+  const fetchFromDB = () => {
+    window.electronAPI
+      .getAllRoutes()
+      .then((data: string) => {
+        // save all the routes to setAllRoutes
+        setAllRoutes(JSON.parse(data))
+      })
+      .catch((err: unknown) => console.log('Problem with db Routes:', err))
+  }
 
+  // select a directory button to select a path
   const handleButtonClick = () => {
     window.electronAPI
       .openFile()
       .then((result: string) => {
-        // const temp = window.electronAPI.openFile().then((result) => {
         // Expect result to be a directory
         window.electronAPI
           .parseFiles(result)
-          .then((result: any) => {
+          .then((result: string[]) => {
             // Expect result to be an array of fetch resources
-            console.log('handlebuttonclick/fetchFromResources', result)
             setResources(result) //    return result;
           })
-          .catch((err: any) => console.log('parseFiles Error:', err))
+          .catch((err: unknown) => console.log('parseFiles Error:', err))
       })
-      .catch((err: any) => console.log('openFile Error: ', err)) // .then()? Maybe save this as temp and chain open file and parse file in one here;
+      .catch((err: unknown) => console.log('openFile Error: ', err))
+    fetchFromDB()
   }
 
-  const fetchFromDB = () => {
-    window.electronAPI
-      .getAllRoutes()
-      .then((data: any) => {
-        setAllRoutes(JSON.parse(data))
-        // console.log('fetchfromDB: getallRoutes', getAllRoutes)
-      })
-      .catch((err: any) => console.log('Problem with db Routes:', err))
-  }
-
-  useEffect(() => {
-    // Console log routes for debugging
-    console.log('routes: ', allRoutes)
-  })
-
-  // this is for the key in the routes being rendered
-  let index = 0
+  // const routeAndResultDisplayHandler = () => {
+  //   setRouteAndResultVisibility((priorState) => {
+  //   !priorState
+  // })
+  // }
 
   return (
     <>
       <header>
         <h1>Middle-Aware</h1>
+
       </header>
       <hr />
       <div id='interface'>
-
         <button className="btn btn-sm" onClick={handleButtonClick}>Select A Directory</button>
-
-        <input type="text" placeholder="PORT #: 9000" className="input input-sm input-bordered w-[10%] max-w-xs" />
+        <div className='reFetch'>
+           <button className="btn btn-sm" onClick={fetchFromDB}>Fetch Tests</button>
+        </div>
       </div>
       <hr />
       <div id="main">
+
+        {/* <div id="routesAndResults"> */}
+        {/* {!routeAndResultVisibility ?
+          <button onClick={routeAndResultDisplayHandler}>Show Routes and Results</button>
+         : */}
         <div id='routesSection'>
           <h2 className='title'>Routes</h2>
-
-          {fetchResources.map((routes: any) => {
-            const databaseRoutes = []
+          {/* iterate through routes */}
+          {fetchResources.map((routes: string) => {
+            const databaseRoutes: string[] = []
             for (const routeData of allRoutes) {
               databaseRoutes.push(routeData.detail)
             }
             const available = databaseRoutes.includes(routes)
-            const error = (routes === '/api/cards')
+            // change this to use and display the status codes on the GUI
+            const error = true
             return (
-              // key is using index above because there is no id; try to use the element index
-              <RouteCards id={routes} detail={routes} onClick={resultHandler} key={index++} available={available} error={error}/>
+              <RouteCards id={routes} detail={routes} onClick={resultHandler} key={fetchResources.indexOf(routes)} available={available} error={error}/>
             )
           })}
 
         </div>
         <div id='resultsSection'>
           <h2 className='title'>Results</h2>
+
             {!results[0]
               ? <>
-              <AwaitingInput/>
-              <div id='checkForData'>
+              {/* <div id='checkForData'>
               <button className="btn btn-sm" onClick={fetchFromDB}>Look For Test Data</button>
-              </div>
+              </div> */}
               </>
-              : results.map((results: any) => (
-            <ResultCards id={results._id} key={results._id} request={results.request} response={results.response} rtt={results.rtt} route_id={results.route_id}/>
+              : results.map((results: Responses) => (
+            <ResultCards id={results._id} key={results._id} request={results.request} response={results.response} rtt={results.rtt} route_id={results.route_id.ref}/>
               ))}
 
         </div>
+        {/* } */}
+        {/* </div> */}
 
       </div>
     </>
