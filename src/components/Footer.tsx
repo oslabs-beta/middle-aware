06b2a880b62c84e52ff0721b4fce70953b5efad7
Felix Leclerc Jr <felixLJR@icloud.com>
2023-05-03 23:48:53 -0400

    Co-authored-by: Jason Breen <jason-breen@users.noreply.github.com> Co-authored-by: justinwmarchant <justinwmarchant@users.noreply.github.com> Co-authored-by: Timothy Chang <tim.chang87@gmail.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { AiFillPauseCircle, AiFillPlayCircle } from 'react-icons/ai'
 const Footer = () => {
   return (
     <div className='flex flex-row w-[100%] bg-white shadow justify-between bottom-0 fixed h-8 px-6'>
-      <p>File Locaton ///////</p>
+      <p>File Location ///////</p>
       <div className='flex flex-row'>
         <p>Status: </p>
         <AiFillPauseCircle className='w-5 text-green-600' />
