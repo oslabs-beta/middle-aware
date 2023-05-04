1448c24ab05d7bbb710cc67564dcea463df97646
Felix Leclerc Jr <felixLJR@icloud.com>
2023-05-03 23:27:19 -0400

    header changes

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { AiFillPauseCircle, AiFillPlayCircle } from 'react-icons/ai'
+
+const Footer = () => {
+  return (
+    <div className='flex flex-row w-[100%]'>
+
+    </div>
+  )
+}
+
+export default Footer
