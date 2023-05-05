669e54940ac2d030ca0af0fb2a48592e992ca0db
Felix Leclerc Jr <felixLJR@icloud.com>
2023-05-05 18:17:42 -0400

    footer layout done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,27 @@
 import React from 'react'
+import { RxDividerVertical } from 'react-icons/rx'
 
+const testName = 'Cards-r-us'
+const testPort = '3000'
 
 const Footer = () => {
   return (
-    <div className='flex flex-row w-[100%] bg-white shadow justify-between bottom-0 fixed h-8 px-6'>
-      <p>File Location ///////</p>
-      <div className='flex flex-row'>
-        <p>Status: </p>
-
+    <div className='flex flex-row w-[100%] bg-white shadow justify-between bottom-0 fixed h-8 px-6 font-light text-md items-center italic'>
+      <div>
+        <p>{testName} </p>
+      </div>
+      <div className='flex flex-row w-[50%] justify-between items-center'>
+      <div>
+        <p>Proxy Port: {testPort} </p>
+      </div>
+        <RxDividerVertical size={15} />
+      <div>
+        <p>Front End Port: {testPort} </p>
+      </div>
+        <RxDividerVertical size={15} />
+      <div>
+        <p>Back End Port: {testPort} </p>
+      </div>
       </div>
     </div>
   )
