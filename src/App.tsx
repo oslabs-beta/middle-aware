b595a04d722872a7e1e24e6623ecc9fdf24d5ebc
Felix Leclerc Jr <felixLJR@icloud.com>
2023-05-07 11:36:42 -0400

    start message centered

--- src/App.tsx
+++ src/App.tsx
@@ -127,26 +127,26 @@ function App() {
           </div>
         </div>
         :
-          <div>
+        <div id='overlay'>
           <div className='start'>
             <div >
-              <div id='start-message'>To get started please select
-                <img src={config_pic} alt="Picture of config button" width={100} id='config_pic' /> above.</div> 
-          <div>
-                If you need assistance see our documentation
+              <div className='start-message'>To get started please select
+                <img src={config_pic} alt="Picture of config button" width={100} id='config_pic' /> above.</div>
+                <br/>
+              <div className='start-message'>
+                If you need assistance see our
                 <button
                   type="button"
                   className="button"
+                  id='docBtn'
                   onClick={openDocs}
                 >
                   <div className='icons'>
-  
+
                   </div>
-                  Config File
+                  Documentation
                 </button>
-          </div>
-              
-        
+              </div>
             </div>
           </div>
         </div>
