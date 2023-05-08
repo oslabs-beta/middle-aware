ee10d7215c30290dde3cb7d13a8427496ebedfd9
Felix Leclerc Jr <felixLJR@icloud.com>
2023-05-08 16:50:22 -0400

    star/stop button dynamically rendered

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,11 +4,13 @@ import { GrConfigure, GrRefresh } from 'react-icons/gr'
 import { RxDividerVertical } from 'react-icons/rx'
 import Toggle from "./Toggle"
 import { BiTestTube } from 'react-icons/bi'
+import { IoStopSharp } from 'react-icons/io5'
 import { HeaderProps } from '../Types'
 
 export default function Header({ config }: HeaderProps) {
 
   const [auto, setAuto] = useState(true)
+  const [start, setStart] = useState(false)
 
   return (
     <div className='fixed w-screen z-50'>
@@ -29,12 +31,15 @@ export default function Header({ config }: HeaderProps) {
           </button>
           <button
             type="button"
-            className="button"
+            className="button w-20"
+            onClick={() => setStart(!start)}
           >
             <div className='icons'>
-              <GrRefresh />
+              {start ? <IoStopSharp /> : <GrRefresh />}
+             
             </div>
-            Start
+            {start ? 'Stop' : 'Start'}
+
           </button>
           <div className='text-slate-300'>
             <RxDividerVertical size={30} />
