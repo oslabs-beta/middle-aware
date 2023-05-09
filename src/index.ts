54afcd92ff9f43f86cd834a5335dba807289f2e8
Tim Chang <tim.chang87@gmail.com>
2023-05-09 18:09:11 -0400

    Fix shell

--- src/index.ts
+++ src/index.ts
@@ -199,9 +199,3 @@ app.whenReady().then(() => {
   ipcMain.handle('startFEParseAndServer', handleStartFEParseAndServer)
   ipcMain.handle('startInstrumentation', handleStartInstrumentation)
 })
-setTimeout(() => {
-  console.log(handleStartFEParseAndServer())
-}, 500)
-setTimeout(() => {
-  console.log(handleStartInstrumentation())
-}, 10000)
