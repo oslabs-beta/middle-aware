3ded79fd5dda5946d8ff9f22137e2864ccd09f59
Felix Leclerc Jr <felixLJR@icloud.com>
2023-05-11 19:23:52 -0400

    keys removed due to conflicts

--- src/components/ResultCards.tsx
+++ src/components/ResultCards.tsx
@@ -35,7 +35,7 @@ const resultCards = ({ id, request, response, rtt, middleware }: any) => {
   return (
 
     <>
-      <div id={id} key={id} className='w-[100%] p-10 rounded-md overflow-auto flex flex-col border border-slate-300 shadow mb-4'
+      <div id={id} className='w-[100%] p-10 rounded-md overflow-auto flex flex-col border border-slate-300 shadow mb-4'
         style={{ background: '#F8FFE5' }}>
 
         <p className='underline underline-offset-4 text-2xl self-center text-slate-600'>Request</p>
@@ -73,7 +73,7 @@ const resultCards = ({ id, request, response, rtt, middleware }: any) => {
         statusCode !== 200 ? <ErrorArrow /> : <SuccessArrow />
       }
 
-      <div id={id} key={id} className='w-[100%] p-10 rounded-md overflow-auto flex flex-col border border-slate-300 shadow mb-44'
+      <div id={id} className='w-[100%] p-10 rounded-md overflow-auto flex flex-col border border-slate-300 shadow mb-44'
         style={{ background: '#F8FFE5' }}>
 
         <p className='underline underline-offset-4 text-2xl self-center text-slate-600'>Response</p>
