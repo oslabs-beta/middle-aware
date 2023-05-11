5b6e0ca87020b04ad232e983b1192eec1f8d81c2
Felix Leclerc Jr <felixLJR@icloud.com>
2023-05-11 13:40:34 -0400

    result cards rendering

--- src/components/ResultCards.tsx
+++ src/components/ResultCards.tsx
@@ -3,38 +3,64 @@ import ErrorArrow from './ErrorArrow'
 import SuccessArrow from './SuccessArrow'
 import { ResultProps } from '../Types'
 
-const resultCards = ({ id, request, response, rtt, middlware }: any) => {
+const resultCards = ({ id, request, response, rtt, middleware }: any) => {
   return (
+    // <>
+    // {/* use a conditional statement to not allow
+    // the first arrow to be rendered perhaps passing the index and making a rule if its [0] */}
+
+    //   <div id={id} className='cards'>
+    //  <h1>Request</h1>
+    //   {/* Endpoint: {request.endpoint}<br />
+    //   Method: {request.method}<br /> */}
+
+    //   <p>Endpoint: {request.endpoint}</p>
+    //   <br />
+    //   <p>Method: {request.method}</p>
+    //   </div>
+
+    //   {
+    //   response.status_code !== 200 ? <ErrorArrow/> : <SuccessArrow/>
+    // }
+
+    // <div id={id} className='cards'>
+    //  <h1>Response</h1>
+    //   <p>Message: {response.message}</p>
+    //   <br />
+    //   {/* <p>Payload: {response.payload}</p>
+    //   <br /> */}
+    //   <p>Status Code: {response.status_code}</p>
+    //   <br />
+    //   <p>Round Trip Time: {rtt}</p>
+    //   </div>
+
+    // </>
     <>
-    {/* use a conditional statement to not allow
-    the first arrow to be rendered perhaps passing the index and making a rule if its [0] */}
+      <div id={id} className='w-[100%] min-h-fit items-center p-10 rounded-md overflow-auto flex flex-row border border-slate-300 shadow mb-4'
+        style={{ background: '#F8FFE5' }}>
+          
 
-      <div id={id} className='cards'>
-     <h1>Request</h1>
-      {/* Endpoint: {request.endpoint}<br />
-      Method: {request.method}<br /> */}
 
-      <p>Endpoint: {request.endpoint}</p>
-      <br />
-      <p>Method: {request.method}</p>
+        {request.method}
       </div>
 
       {
-      response.status_code !== 200 ? <ErrorArrow/> : <SuccessArrow/>
-    }
+        middleware.map((func: string) => {
+          return (<div id={id} key={middleware.indexOf(func)} className='w-[100%] min-h-fit items-center p-10 rounded-md overflow-auto flex flex-row border border-slate-300 shadow mb-4'
+            style={{ background: '#F8FFE5' }}>
+            {func}
+          </div>)
+        })
 
-    <div id={id} className='cards'>
-     <h1>Response</h1>
-      <p>Message: {response.message}</p>
-      <br />
-      {/* <p>Payload: {response.payload}</p>
-      <br /> */}
-      <p>Status Code: {response.status_code}</p>
-      <br />
-      <p>Round Trip Time: {rtt}</p>
+      }
+
+      <div id={id} className='w-[100%] min-h-fit items-center p-10 rounded-md overflow-auto flex flex-row border border-slate-300 shadow mb-4'
+        style={{ background: '#F8FFE5' }}>
+        {response.body}
+        {rtt}
       </div>
-
     </>
+
   )
 }
 
