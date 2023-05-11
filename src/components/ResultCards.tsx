e40fb34df66fbe4c2aace80c63f54f6ce4c17c78
Felix Leclerc Jr <felixLJR@icloud.com>
2023-05-11 18:36:40 -0400

    all result cards rendering

--- src/components/ResultCards.tsx
+++ src/components/ResultCards.tsx
@@ -5,52 +5,76 @@ import { ResultProps } from '../Types'
 
 const resultCards = ({ id, request, response, rtt, middleware }: any) => {
 
+  let bodyValues
+  let bodyKeys
+  let paramsKeys
+  let paramsValues
+  let queryKeys
+  let queryValues
+  let time
+  let statusCode
+
+
   if (typeof request.body === 'object' && request.body !== null) {
-    const bodyKeys = Object.keys(request.body)
-    const bodyValues = Object.values(request.body)
+    bodyKeys = Object.keys(request.body)
+    bodyValues = Object.values(request.body)
   }
 
   if (typeof request.params === 'object' && request.params !== null) {
-    const paramsKeys = Object.keys(request.params)
-    const paramsValues = Object.values(request.params)
+    paramsKeys = Object.keys(request.params)
+    paramsValues = Object.values(request.params)
   }
 
   if (typeof request.query === 'object' && request.query !== null) {
-    const queryKeys = Object.keys(request.query)
-    const queryValues = Object.values(request.query)
+    queryKeys = Object.keys(request.query)
+    queryValues = Object.values(request.query)
   }
-  if (rtt !== undefined) { const time = `${rtt.slice(0, 5)} ms` }
+  if (rtt !== undefined) { time = `${rtt.slice(0, 5)} ms` }
+  if (typeof request.body === 'object' && request.body?.status_code !== null) {statusCode = request.body.status_code}
 
   return (
 
     <>
-      <div id={id} className='w-[100%] min-h-fit items-center p-10 rounded-md overflow-auto flex flex-row border border-slate-300 shadow mb-4'
+      <div id={id} className='w-[100%] p-10 rounded-md overflow-auto flex flex-col border border-slate-300 shadow mb-4'
         style={{ background: '#F8FFE5' }}>
 
-        <h2>Request</h2>
-        <div> <p>Method:</p> {request.method}</div>
-        <div> <p>Body:</p> {typeof request.body}</div>
-        <div> <p>Params:</p> {typeof request?.params}</div>
-        <div> <p>Query:</p> {typeof request?.method}</div>
+        <p className='underline underline-offset-4 text-2xl self-center text-slate-600'>Request</p>
 
-
+        <div className='ReqRespCards'> <p className='card-sub-titles'>Method: </p> {request.method}</div>
+        <hr className="horizontalRule" />
+        <div className='ReqRespCards'> <p className='card-sub-titles'>Body: </p> {bodyValues} </div>
+        <hr className="horizontalRule" />
+        <div className='ReqRespCards'> <p className='card-sub-titles'>Params: </p> {paramsValues}</div>
+        <hr className="horizontalRule" />
+        <div className='ReqRespCards'> <p className='card-sub-titles'>Query: </p> {queryValues}</div>
       </div>
 
       {
         middleware.map((func: string) => {
-          return (<div id={id} key={middleware.indexOf(func)} className='w-[100%] min-h-fit items-center p-10 rounded-md overflow-auto flex flex-row border border-slate-300 shadow mb-4'
+          return (<div id={id} key={middleware.indexOf(func)} className='w-[60%] min-h-fit items-center p-10 rounded-md overflow-auto flex flex-col border border-slate-300 shadow mb-4'
             style={{ background: '#F8FFE5' }}>
-            {func}
+            <p className='underline underline-offset-4 text-xl self-center text-slate-600 mb-3'>Function:{' '}{`${middleware.indexOf(func)+1}`}</p>
+            <div className=' text-slate-600 text-lg'>{func}</div> 
           </div>)
         })
 
       }
 
-      <div id={id} className='w-[100%] min-h-fit items-center p-10 rounded-md overflow-auto flex flex-row border border-slate-300 shadow mb-4'
+      <div id={id} className='w-[100%] p-10 rounded-md overflow-auto flex flex-col border border-slate-300 shadow mb-4'
         style={{ background: '#F8FFE5' }}>
-        {response.body}
-        {rtt}
+
+        <p className='underline underline-offset-4 text-2xl self-center text-slate-600'>Response</p>
+
+      {/* <div id={id} className='w-[100%] min-h-fit items-center p-10 rounded-md overflow-auto flex flex-row border border-slate-300 shadow mb-4'
+        style={{ background: '#F8FFE5' }}> */}
+          <div className='ReqRespCards'> <p className='card-sub-titles'>Round Trip Time:</p> {time}</div>
+          <hr className="horizontalRule" />
+          <div className='ReqRespCards'> <p className='card-sub-titles'>Status Code: </p> {statusCode} </div>
+          <hr className="horizontalRule" />
+          <div className='ReqRespCards'> <p className='card-sub-titles'>Body: </p> {response.body} </div>
       </div>
+
+      {/* </div> */}
     </>
 
     // <>
